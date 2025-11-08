8dfc8542851f70729d66f163d8be07eee8e26002
Daney <daneyyhh64@gmail.com>
2025-11-08 20:31:03 +0530

    Add FiveM resource manifest file

---
+++ example-resource/fxmanifest.lua
@@ -0,0 +1,26 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Your Name'
+description 'Example FiveM Resource'
+version '1.0.0'
+
+-- Client Scripts
+client_scripts {
+    'client/*.lua'
+}
+
+-- Server Scripts
+server_scripts {
+    'server/*.lua'
+}
+
+-- Shared Scripts
+shared_scripts {
+    'shared/*.lua'
+}
+
+-- Files
+files {
+    'config.json'
+}
